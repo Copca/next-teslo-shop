305a3252a0a97014404e883611e12089d32cddcd
Ernest <dite_malacoda@hotmail.com>
2022-11-07 11:26:19 -0600

    Chip component sin bordes

--- components/ui/Chip.tsx
+++ components/ui/Chip.tsx
@@ -9,12 +9,12 @@ export const Chip: FC<Props> = ({ pagado = false }) => {
 	return (
 		<>
 			{pagado ? (
-				<div className='inline-flex items-center text-green-500 font-bold border-2 border-green-500 rounded-full px-3 py-2 space-x-3 mb-4'>
+				<div className='inline-flex items-center text-green-500 font-bold border-2 border-green-500 rounded-full px-3 py-2 space-x-3'>
 					<MdCreditScore className='text-xl' />
 					<p>Pagado</p>
 				</div>
 			) : (
-				<div className='inline-flex items-center text-red-500 font-bold border-2 border-red-500 rounded-full px-3 py-2 space-x-3 mb-4'>
+				<div className='inline-flex items-center text-red-500 font-bold border-2 border-red-500 rounded-full px-3 py-2 space-x-3'>
 					<MdOutlineCreditCardOff className='text-xl' />
 					<p>Pendiente de Pago</p>
 				</div>
