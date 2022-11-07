74da94d7e88b36d3cc73d1d73ec08a86d8446d70
Ernest <dite_malacoda@hotmail.com>
2022-11-07 13:22:51 -0600

    registerPage

---
+++ pages/auth/register.tsx
@@ -0,0 +1,65 @@
+import { FormEvent } from 'react';
+import { NextPage } from 'next';
+import Link from 'next/link';
+import { AiFillGithub } from 'react-icons/ai';
+
+import { AuthLayout } from '../../components/layouts';
+import { InputLogin } from '../../components/ui';
+
+const RegisterPage: NextPage = () => {
+	const onSubmitForm = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+	};
+
+	return (
+		<AuthLayout tittle={'Crear cuenta'}>
+			<div className='container'>
+				<div className='max-w-lg mx-auto bg-white border shadow rounded-md p-8'>
+					<h1 className='text-2xl font-bold mb-8'>Crear cuenta</h1>
+
+					<form onSubmit={onSubmitForm}>
+						<InputLogin type='text' labelText='Nombre' className='mb-4' />
+						<InputLogin type='email' labelText='Email' className='mb-4' />
+						<InputLogin
+							type='password'
+							labelText='Contraseña'
+							className='mb-4'
+						/>
+
+						<button
+							type='submit'
+							className=' btn bg-sky-500 hover:bg-sky-600 w-full my-8'
+							data-mdb-ripple='true'
+							data-mdb-ripple-color='light'
+						>
+							Ingresar
+						</button>
+
+						<div className='flex justify-end'>
+							<Link
+								href={'/auth/login'}
+								className='underline text-slate-500 hover:text-slate-700 transition-colors'
+							>
+								Ya tengo una cuenta, iniciar sesión
+							</Link>
+						</div>
+
+						<hr className='my-4' />
+
+						<div className='flex justify-center'>
+							<button
+								className='btn text-gray-800 hover:bg-gray-100 border border-gray-500 gap-2 w-full mt-8'
+								data-mdb-ripple='true'
+								data-mdb-ripple-color='dark'
+							>
+								<AiFillGithub className='text-lg' /> GitHub
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</AuthLayout>
+	);
+};
+
+export default RegisterPage;
