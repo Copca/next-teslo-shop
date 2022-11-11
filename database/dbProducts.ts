4451ade0b721d12618c33d4c5810b6d5ab275af1
Ernest <dite_malacoda@hotmail.com>
2022-11-11 11:50:15 -0600

    api getProductByTerm

--- database/dbProducts.ts
+++ database/dbProducts.ts
@@ -1,3 +1,7 @@
+/**
+ * consultas a la DB deste el backend para no utilizar la API, ya que sería más tardada la consulta
+ */
+
 import { db } from './';
 import { Product } from '../models';
 import { IProduct } from '../interfaces';
@@ -24,4 +28,26 @@ export const getAllProductsSlugs = async (): Promise<ProductSlug[]> => {
 	await db.disconnect();
 
 	return slugs;
+};
+
+export const getProductsByTerm = async (term: string): Promise<IProduct[]> => {
+	term = term.toString().toLowerCase();
+
+	await db.connect();
+	const products = await Product.find({
+		$text: { $search: term }
+	})
+		.select('title images price inStock slug -_id')
+		.lean();
+	await db.disconnect();
+
+	return products;
+};
+
+export const getAllProducts = async (): Promise<IProduct[]> => {
+	await db.connect();
+	const products = await Product.find().lean();
+	await db.disconnect();
+
+	return JSON.parse(JSON.stringify(products));
 };
