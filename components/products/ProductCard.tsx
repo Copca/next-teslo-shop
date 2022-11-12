cd6e48792b7d0116ff3eeb09a7c1ef4aa764c3ae
Ernest <dite_malacoda@hotmail.com>
2022-11-12 09:23:20 -0600

    etiqueta de disponibilidad de producto

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -17,28 +17,36 @@ export const ProductCard: FC<Props> = ({ product }) => {
 			prefetch={false}
 			onMouseEnter={() => setIsHovered(true)}
 			onMouseLeave={() => setIsHovered(false)}
-			className='animate-fadeIn flex flex-col items-center'
+			className=' flex flex-col items-center'
 		>
-			<button data-mdb-ripple='true'>
-				<Image
-					src={
-						isHovered
-							? `/products/${product.images[0]}`
-							: `/products/${product.images[1]}`
-					}
-					width={300}
-					height={300}
-					alt={`imagen ${product.title}`}
-					priority
-					className={`rounded-md shadow ${
-						isHovered && 'animate-[fadeIn_1.5s_ease-in-out_1]'
-					}`}
-				/>
-			</button>
+			<div className='relative'>
+				{!product.inStock && (
+					<div className='absolute top-2 right-2 bg-slate-900 text-white text-sm text-center rounded-full py-1 px-2 z-10'>
+						No hay disponibles
+					</div>
+				)}
 
-			<div className='text-left px-4'>
-				<h6>{product.title}</h6>
-				<p>$ {product.price}</p>
+				<button data-mdb-ripple='true' className='relative'>
+					<Image
+						src={
+							isHovered
+								? `/products/${product.images[0]}`
+								: `/products/${product.images[1]}`
+						}
+						width={300}
+						height={300}
+						alt={`imagen ${product.title}`}
+						priority
+						className={`rounded-md shadow ${
+							isHovered && 'animate-[fadeIn_1.5s_ease-in-out_1]'
+						}`}
+					/>
+				</button>
+
+				<div className='text-left px-4'>
+					<h6>{product.title}</h6>
+					<p>$ {product.price}</p>
+				</div>
 			</div>
 		</Link>
 	);
--- components/products/ProductList.tsx
+++ components/products/ProductList.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const ProductList: FC<Props> = ({ products }) => {
 	return (
-		<div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-8 mt-8 text-slate-700 font-bold'>
+		<div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-8 mt-8 text-slate-700 font-bold animate-fadeIn'>
 			{products.map((product) => (
 				<div key={product.slug} className='flex flex-col items-center'>
 					<ProductCard product={product} />
