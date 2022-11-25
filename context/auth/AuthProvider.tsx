7bf39cd9dc7187f4e6a014a41ee27299427d3a01
Ernest <dite_malacoda@hotmail.com>
2022-11-25 14:03:54 -0600

    limpiar Cookies en Logout

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -100,6 +100,15 @@ export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
 		Cookies.remove('token');
 		Cookies.remove('cart');
 
+		Cookies.remove('firstName');
+		Cookies.remove('lastName');
+		Cookies.remove('address');
+		Cookies.remove('address2');
+		Cookies.remove('zip');
+		Cookies.remove('city');
+		Cookies.remove('country');
+		Cookies.remove('phone');
+
 		// refresh borra todo el state de la aplicación
 		router.reload();
 	};
